e51e16254cdc927d5465992bd738cb440f35af57
AJ Ashik <uniquecollection163@gmail.com>
2024-11-01 08:12:11 +0600

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,14 +1,16 @@
 // /api/stream.js
 import https from 'https';
-import fs from 'fs';
-import path from 'path';
 
 export default function handler(req, res) {
     const streamUrl = req.query.id;
 
-    // headers.json ফাইল থেকে হেডার ডেটা পড়ুন
-    const headersPath = path.resolve('https://raw.githubusercontent.com/byte-capsule/Toffee-Channels-Link-Headers/refs/heads/main/toffee_channel_data.json');
-    const headers = JSON.parse(fs.readFileSync(headersPath, 'utf8'));
+    // headers সরাসরি কোডের মধ্যে ইনলাইন করা হয়েছে
+    const headers = {
+        "Host": "bldcmprod-cdn.toffeelive.com",
+        "Cookie": "Edge-Cache-Cookie=URLPrefix=aHR0cHM6Ly9ibGRjbXByb2QtY2RuLnRvZmZlZWxpdmUuY29tLw:Expires=1730578747:KeyName=prod_linear:Signature=RUS2g1xB-hHX-efOnZwgREM4EZKKL8-hx1nTFvGqnBf2wQN-f2v3-g0liH9dGCBoOGu1FxA9e--Ud3Z_xE5mCw",
+        "User-Agent": "Toffee (Linux;Android 14) AndroidXMedia3/1.1.1/64103898/4d2ec9b8c7534adc",
+        "Client-API-Header": "angM1aXCHQLmmSW6cDlpXMD6tLdwnhMoUeaBBFKmd98bX6Vrae5xCMbm4gg0+u33rnxeGQDZNr2GD1tW0cWwKEpWimNlGqXVQGhpiIBz1JFxN+OxXcQqaMPrjwUhCyI5mO1DGyNv18+Z2EpmHtVnLzV9SrGsQWu4oRKjxE8QIMsRs6LrvL6hWGPlOGQke/qb5QxQZNetPzI39jHhX7Zi2XrCMIT4a+gk2Wu1c3wIybwkqknPcTp4Bj1cEF3Q+q1dV05SBhzpEDfoR2BLyQ6dV3LvmY6MNKxbUjby7hMsg35lFl2Df2mZsr7C27309w/qWi8lLXDjB7B1MozIGKn8rw3bXY5YlrPKBKztyiisAjQQi7kc5ISXyGSwRmhciwkciuitsSL0LlqHY7/Qkkh71EtaK3XEgVpLdH8zRCsTwfu1iIVPiDwTycuuBy4XWkcNnd0iLB35yftQpiL8HfpO2jQnrAwzePxszJ7mewVG+M0P/qyTBD52NkPR8uW0AZmDKp5LHTCGf7sqldDzpZvU+gsSdvtsBUcmHzjINGEoyXk="
+    };
 
     const options = { headers };
 
